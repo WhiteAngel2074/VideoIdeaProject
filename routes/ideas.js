33bb66da38f9d260c19162a2b0aaf8db78ea6121
Hamdi Chebbi <hamdi.chebbi@outlook.fr>
2018-04-15 17:12:34 +0100

    init

---
+++ routes/ideas.js
@@ -0,0 +1,93 @@
+var express = require("express");
+var router = express.Router();
+var bodyParser = require("body-parser");
+var idea = require("../models/Idea");
+var methodOverride = require("method-override");
+
+/* GET ideas listing. */
+router.get("/", function(req, res, next) {
+  idea
+    .find({})
+    .sort({ date: "desc" })
+    .then(ideas => {
+      res.render("ideas/index", {
+        ideas: ideas
+      });
+    });
+});
+
+/* Add an Idea FORM  */
+router.get("/add", (req, res) => {
+  res.render("ideas/add");
+});
+
+// Edit Page
+router.get("/edit/:id", (req, res) => {
+  idea
+    .findOne({
+      _id: req.params.id
+    })
+    .then(idea => {
+      res.render("ideas/edit", {
+        idea: idea
+      });
+    });
+});
+
+// validation & add idea
+router.post("/", (req, res) => {
+  let errors = [];
+
+  if (!req.body.title) {
+    errors.push({ text: "Please add a title" });
+  }
+  // if (!req.body.details) {
+  //   errors.push({ text: "Please add some details" });
+  // }
+  if (errors.length > 0) {
+    res.render("ideas/add", {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    {
+      const newUser = {
+        title: req.body.title,
+        details: req.body.details
+      };
+      new idea(newUser).save().then(idea => {
+        req.flash("success_msg", "Video Idea added");
+        res.redirect("/ideas");
+      });
+    }
+  }
+});
+
+// EDIT PUT idea
+router.put("/:id", (req, res) => {
+  idea
+    .findOne({
+      _id: req.params.id
+    })
+    .then(idea => {
+      // new values
+      idea.title = req.body.title;
+      idea.details = req.body.details;
+
+      idea.save().then(idea => {
+        req.flash("success_msg", "Video Idea updated");
+        res.redirect("/ideas");
+      });
+    });
+});
+
+// DELETE
+router.delete("/:id", (req, res) => {
+  idea.remove({ _id: req.params.id }).then(() => {
+    req.flash('success_msg', 'Video Idea removed')
+    res.redirect("/ideas");
+  });
+});
+
+module.exports = router;
